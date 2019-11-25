67c1a8e8f2725a22f2ca496f3d99c86edca2289c
ezekielbarnett <ezekbarnett@gmail.com>
2019-11-25 18:29:48 +0100

    basic version - some bugs remain

--- py/model.py
+++ py/model.py
@@ -15,33 +15,32 @@ def distance_vectors_pairwise(anchor, positive, negative , squared = True):
     d_p_n = p_sq + n_sq - 2*torch.sum(positive * negative, dim = 1)
 
     if not squared:
-     d_a_p = torch.sqrt(d_a_p + eps)
-     d_a_n = torch.sqrt(d_a_n + eps)
-     d_p_n = torch.sqrt(d_p_n + eps)
+        d_a_p = torch.sqrt(d_a_p + eps)
+        d_a_n = torch.sqrt(d_a_n + eps)
+        d_p_n = torch.sqrt(d_p_n + eps)
 
     return d_a_p, d_a_n, d_p_n
 
 class Model(nn.Module):
- def __init__(self, base_model, num_classes, embedding_size = 128, lr = 0.001):
-  super(Model, self).__init__()
-  self.base_model = base_model
-  self.num_classes = num_classes
-  self.embedder = nn.Linear(base_model.output_size, embedding_size)
-  self.lr = lr
+    def __init__(self, base_model, num_classes, embedding_size = 128, lr = 0.001):
+        super(Model, self).__init__()
+        self.base_model = base_model
+        self.num_classes = num_classes
+        self.embedder = nn.Linear(base_model.output_size, embedding_size)
+        self.lr = lr
 
- def forward(self, input):
-  return self.embedder(F.relu(self.base_model(input).view(len(input), -1)))
+    def forward(self, input):
+        return self.embedder(F.relu(self.base_model(input).view(len(input), -1)))
  
- criterion = None
+     criterion = None
 
 class WeightedTriplet(Model):
- def forward(self, input):
-  return F.normalize(Model.forward(self, input))
+    def forward(self, input):
+        return F.normalize(Model.forward(self, input))
 
- def criterion(self, a_emb, p_emb, n_emb, p_w, n_w, margin = 1.0):
-  (d_a_p, d_a_n, _) = distance_vectors_pairwise(a_emb, p_emb, n_emb)
-  loss = torch.clamp(margin + d_a_p - d_a_n, min=0.0)
-  loss = loss * p_w.float()
-  loss = torch.mean(loss)
-
-  return loss
+    def criterion(self, a_emb, p_emb, n_emb, p_w, n_w, margin = 1.0):
+        (d_a_p, d_a_n, _) = distance_vectors_pairwise(a_emb, p_emb, n_emb)
+        loss = torch.clamp(margin + d_a_p - d_a_n, min=0.0)
+        loss = loss * p_w.float()
+        loss = torch.mean(loss)
+        return loss
